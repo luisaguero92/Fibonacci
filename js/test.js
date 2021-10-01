1953fc386ff26586779cefdf4206c2fba49b5c29
luisaguero92 <luisaguero92@gmail.com>
2021-09-30 23:01:34 -0300

    last commit

--- js/test.js
+++ js/test.js
@@ -1,27 +1,15 @@
-// program to generate fibonacci series up to n terms
-
-// take input from the user
-const number = parseInt(prompt('Enter the number of terms: '));
-let n1 = 0, n2 = 1, nextTerm;
-
-console.log('Fibonacci Series:');
-
-for (let i = 1; i <= number; i++) {
-    console.log(n1);
-    nextTerm = n1 + n2;
-    n1 = n2;
-    n2 = nextTerm;
-}
 
 // Cantidad de números que deseamos imprimir
+const number = parseInt(prompt('Enter the number of terms: '));
+
 //var limit = number;
 
 var fibo = [0, 1];
-    console.log('Fibo 3')
+    console.log('Serie de Fibonacci')
     console.log(fibo);
     for (let i = 2; i <= number; i++) {
         fibo.push(fibo[i - 1] + fibo[i - 2]);
-        document.write(fibo[i]);
+        
     }
-
-
+    document.write(fibo);
+alert("La serie de fibonacci es la siguiente: " + fibo)
